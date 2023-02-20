bfdadd151b2089c905b09eab3ee26a7a08447f4e
Tomas Fuentes <fuentestomas280@gmail.com>
2023-02-20 09:31:20 -0300

    first commit

---
+++ Portafolio/Servicios/RepositorioProyectos.cs
@@ -0,0 +1,56 @@
+﻿using Portafolio.Models;
+
+namespace Portafolio.Servicios
+{
+
+
+
+    public interface IRepositorioProyectos
+    {
+        List<Proyecto> ObtenerProyectos();
+    }
+
+
+    public class RepositorioProyectos: IRepositorioProyectos
+    {
+
+        public List<Proyecto> ObtenerProyectos()
+        {
+            return new List<Proyecto>() {
+                    new Proyecto
+                    {
+                        Titulo = "Amazon",
+                        Descripcion="Realizado en asp.net core",
+                        Link="https://www.google.com/",
+                        ImagenURL="/imagenes/amazon.png"
+                    },
+                      new Proyecto
+                    {
+                        Titulo = "Amazon1",
+                        Descripcion="Realizado en asp.net core",
+                        Link="https://www.google.com/",
+                        ImagenURL="/imagenes/steam.png"
+                    },
+                        new Proyecto
+                    {
+                        Titulo = "Amazon2",
+                        Descripcion="Realizado en asp.net core",
+                        Link="https://www.google.com/",
+                        ImagenURL="/imagenes/nyt.png"
+                    },
+                         new Proyecto
+                    {
+                        Titulo = "Amazon2",
+                        Descripcion="Realizado en asp.net core",
+                        Link="https://www.google.com/",
+                        ImagenURL="/imagenes/reddit.png"
+                    }
+            };
+
+
+        }
+
+
+
+    }
+}
